e4d2b1549e56ea801cc52ced1c77c366752e8f7e
Jan Kornienko <kor.jan@outlook.com>
2020-09-22 21:30:21 +0200

    not working authentication

---
+++ questions.js
@@ -0,0 +1,20 @@
+var questions = [
+	{
+		question: "Otázka 1 je otázka 1?",
+		answers: [
+			{ id: 0, answer: "Odpověď 1", result: true },
+			{ id: 1, answer: "Odpověď 2", result: false },
+			{ id: 2, answer: "Odpověď 3", result: false }
+		],
+		selected: []
+	},
+	{
+		question: "Otázka 2 je otázka 2?",
+		answers: [
+			{ id: 0, answer: "Odpověď 1", result: false },
+			{ id: 1, answer: "Odpověď 2", result: true },
+			{ id: 2, answer: "Odpověď 3", result: false }
+		],
+		selected: []
+	},
+]
--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 var vm = new Vue({
-    el: "#app",
-    data: {
-
-    },
-    methods: {
-        
-    }
+	el: "#app",
+	data: {
+		questions: questions
+	},
+	methods: {
+		onSubmit() {
+			this.questions.forEach(question => {
+				question.answers.forEach(answer => {
+					question.selected.forEach(usrAns => {
+						if (usrAns == answer.id) { usrSel = true; }
+						if (usrSel == answer.result) { ansCor = true;}
+						else { ansCor = false }
+					})
+				})
+				
+			});
+		}
+	}
 });
