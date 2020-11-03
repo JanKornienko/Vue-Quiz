00106efc09f400057f8e2c119fab95acef3c89c6
Jan Kornienko <kor.jan@outlook.com>
2020-11-03 20:25:33 +0100

    Editor for questions

--- script.js
+++ script.js
@@ -2,23 +2,57 @@ var vm = new Vue({
 	el: "#app",
 	data: {
 		questions: questionsList,
-		points: 0
+		questionInput: "",
+		answerInput: "",
+		answersInputList: [],
+		correctInput: 0,
+		correctInputList: [],
 	},
 	methods: {
 		onSubmit() {
 			// Authentication of answers
-			//this.points = 0
 			this.questions.forEach(question => {
 				question.answersList.forEach(answer => {
 					answer.correct = question.correct.includes(answer.id);
 					if ((question.selected.includes(answer.id)) && (answer.correct == false)) {	answer.wrong = true; }
 				})
-				//if (question.answersList.includes(wrong = true)) { points++; }
-				if (question.selected == question.correct) {
-					points++;
-				}
 			});
-			console.log(points);
+		},
+
+		newAnswer() {
+			// Write new answer
+			if (this.answerInput != "") {
+				this.answersInputList.push({
+					id: this.answersInputList.length,
+					answer: this.answerInput,
+					correct: false,
+					wrong: false
+				});
+				// Checkbox for correct answer
+				if (document.getElementById("correctCheckbox").checked = true) {
+					this.correctInputList.push(this.correctInput);
+				};
+				this.answerInput = "";
+				this.correctInput = 0;
+			} else { alert("Empty answer input"); }
+		},
+
+		newQuestion() {
+			if ((this.questionInput != "") && (this.correctInputList.length > 0) && (this.answersInputList.length > 0)) {
+				this.questions.push({
+					question: this.questionInput,
+					answersList: this.answersInputList,
+					correct: this.correctInputList,
+					selected: []
+				});
+				this.questionInput = "";
+				this.answersInputList = [];
+				this.correctInputList = [];
+			} else {
+				if (this.questionInput == "") { alert("Empty question input"); }
+				if (this.answersInputList.length <= 0) { alert("You didn't add any answer for the question") }
+				if (this.correctInputList.length <= 0) { alert("Not selected at lease one correct answer"); }
+			}
 		}
 	}
 });
